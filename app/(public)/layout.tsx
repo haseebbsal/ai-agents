82ffda3e500e54b80a21ca99f2922e758908d837
Haseeb <haseeb.salman1234@outlook.com>
2024-11-20 05:00:34 +0500

    push files

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,9 +1,5 @@
-'use client'
-import HomeNavbar from "@/components/common/home-navbar";
-import BaseAutoComplete from "@/components/form/base-autocomplete";
-import BaseInput from "@/components/form/base-input";
 import NavProvider from "@/providers/nav-provider";
-import { createContext, ReactNode, useState } from "react";
+import { ReactNode } from "react";
 export default function MainLayout({ children }: { children: ReactNode }) {
     return (
         <>
