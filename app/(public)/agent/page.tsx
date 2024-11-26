56ebf12361ad41617853716f4a33018aa242dcde
Haseeb <haseeb.salman1234@outlook.com>
2024-11-26 06:44:47 +0500

    files

--- app/(public)/agent/page.tsx
+++ app/(public)/agent/page.tsx
@@ -51,7 +51,7 @@ const agents = [
     {
         imgSrc: "/agents/chat.svg",
         agentText: "Personalized Recommendation",
-        info:"Personalized Recommendation Agent assists customer service agents and sales representatives by recommending relevant products based on customer responses and characteristics."
+        info: "Personalized Recommendation Agent assists customer service agents and sales representatives by recommending relevant products based on customer responses and characteristics."
     }
 ]
 
@@ -102,25 +102,25 @@ export default function Home() {
             }
         })
 
-        if(Object.keys(e).includes('characteristics')){
-            const customer_segment=e['customer_segment']
-            if(customer_segment=='Business'){
-                Object.entries(e['characteristics']).forEach((m)=>{
-                    if(m[0]!='industry' && m[0]!='risk_concern'){
+        if (Object.keys(e).includes('characteristics')) {
+            const customer_segment = e['customer_segment']
+            if (customer_segment == 'Business') {
+                Object.entries(e['characteristics']).forEach((m) => {
+                    if (m[0] != 'industry' && m[0] != 'risk_concern') {
                         delete e['characteristics'][m[0]]
                     }
                 })
             }
-            if(customer_segment=='Specialized'){
-                Object.entries(e['characteristics']).forEach((m)=>{
-                    if(m[0]!='type_of_entity' && m[0]!='specific_needs'){
+            if (customer_segment == 'Specialized') {
+                Object.entries(e['characteristics']).forEach((m) => {
+                    if (m[0] != 'type_of_entity' && m[0] != 'specific_needs') {
                         delete e['characteristics'][m[0]]
                     }
                 })
             }
-            if(customer_segment=='Individual'){
-                Object.entries(e['characteristics']).forEach((m)=>{
-                    if(m[0]!='type' && m[0]!='interest'){
+            if (customer_segment == 'Individual') {
+                Object.entries(e['characteristics']).forEach((m) => {
+                    if (m[0] != 'type' && m[0] != 'interest') {
                         delete e['characteristics'][m[0]]
                     }
                 })
@@ -183,7 +183,7 @@ export default function Home() {
                         </>}
                         {getValues().customer_segment == 'Specialized' && <>
                             <BaseAgentInput name="characteristics.type_of_entity" label='Type Of Specialized Entity' labelPlacement="outside" control={control} rules={{ required: "Enter Type Of Specialized Entity" }} placeholder="What type of specialized entity are you? (e.g., Airlines, Non-Profit Organization, Educational Institution)?" />
-                            <BaseAgentInput name="characteristics.specific_needs" label='Primary Insurance Need' labelPlacement="outside" control={control} rules={{ required: "Enter Primary Insurance Need" }} placeholder="What is the primary insurance need for your entity (e.g., Aviation Insurance, Liability, Health)?" /> 
+                            <BaseAgentInput name="characteristics.specific_needs" label='Primary Insurance Need' labelPlacement="outside" control={control} rules={{ required: "Enter Primary Insurance Need" }} placeholder="What is the primary insurance need for your entity (e.g., Aviation Insurance, Liability, Health)?" />
                         </>}
                     </div>
                 }
@@ -205,7 +205,9 @@ export default function Home() {
                                         </div>
                                         <div className="flex gap-4">
                                             <p className="font-semibold">Task Name:</p>
-                                            <p>{e.name}</p>
+                                            <p>{e.name.replaceAll('_', ' ').split(' ').map((word: any) =>
+                                                word.charAt(0).toUpperCase() + word.slice(1)
+                                            ).join(' ')}</p>
                                         </div>
                                         <div className="flex flex-col gap-4">
                                             <Markdown>{e.raw}</Markdown>
@@ -222,7 +224,9 @@ export default function Home() {
                                 </div>
                                 <div className="flex gap-4">
                                     <p className="font-semibold">Task Name:</p>
-                                    <p>{e.name}</p>
+                                    <p>{e.name.replaceAll('_', ' ').split(' ').map((word: any) =>
+                                        word.charAt(0).toUpperCase() + word.slice(1)
+                                    ).join(' ')}</p>
                                 </div>
                                 <div className="flex flex-col gap-2">
 
@@ -256,7 +260,9 @@ export default function Home() {
                                 </div>
                                 <div className="flex gap-4">
                                     <p className="font-semibold">Task Name:</p>
-                                    <p>{e.name.replaceAll('_', ' ')}</p>
+                                    <p>{e.name.replaceAll('_', ' ').split(' ').map((word: any) =>
+                                        word.charAt(0).toUpperCase() + word.slice(1)
+                                    ).join(' ')}</p>
                                 </div>
                                 <div className="flex flex-col gap-4">
                                     <Markdown>{e.raw}</Markdown>
@@ -266,25 +272,27 @@ export default function Home() {
                     )
                     )}
                     {
-                        agent=='10' && data?.map((e: any, number: number) =>
-                            (
-                                <div key={number} className="flex flex-col shadow-lg p-4 rounded-lg gap-4">
+                        agent == '10' && data?.map((e: any, number: number) =>
+                        (
+                            <div key={number} className="flex flex-col shadow-lg p-4 rounded-lg gap-4">
+                                <div className="flex flex-col gap-4">
+                                    <div className="flex gap-4">
+                                        <p className="font-semibold">Agent:</p>
+                                        <p>{number == 0 ? "Competitor Data Retrieval" : number == 1 ? "Market Positioning" : number == 3 ? "Performance Matrix Analysis" : "Competitor Comparison"}</p>
+                                    </div>
+                                    <div className="flex gap-4">
+                                        <p className="font-semibold">Task Name:</p>
+                                        <p>{e.name.replaceAll('_', ' ').split(' ').map((word: any) =>
+                                            word.charAt(0).toUpperCase() + word.slice(1)
+                                        ).join(' ')}</p>
+                                    </div>
                                     <div className="flex flex-col gap-4">
-                                        <div className="flex gap-4">
-                                            <p className="font-semibold">Agent:</p>
-                                            <p>{number == 0 ? "Competitor Data Retrieval" : number == 1 ? "Market Positioning" : number == 3 ? "Performance Matrix Analysis" : "Competitor Comparison"}</p>
-                                        </div>
-                                        <div className="flex gap-4">
-                                            <p className="font-semibold">Task Name:</p>
-                                            <p>{e.name.replaceAll('_', ' ')}</p>
-                                        </div>
-                                        <div className="flex flex-col gap-4">
-                                            <Markdown>{e.raw}</Markdown>
-                                        </div>
+                                        <Markdown>{e.raw}</Markdown>
                                     </div>
                                 </div>
-                            )
-                            )
+                            </div>
+                        )
+                        )
                     }
                 </div>
 
