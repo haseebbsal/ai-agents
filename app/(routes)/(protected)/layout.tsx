a1f0ec8319cda13f31fd1e8209eee46b68726fcd
Haseeb <haseeb.salman1234@outlook.com>
2024-12-01 05:32:37 +0500

    push

--- app/(routes)/(protected)/layout.tsx
+++ app/(routes)/(protected)/layout.tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 import Cookies from 'js-cookie'
 export default async function ProtectedLayout({ children }: { children: ReactNode }) {
-    // console.log('coookieee',Cookies.get('accessToken'))
+    console.log('coookieee',Cookies.get('accessToken'))
     return (
         <>
             {children}
