56e3a322a6126f8a303fa28ecabeb9e1f3927ebd
Haseeb <haseeb.salman1234@outlook.com>
2024-12-05 02:10:14 +0500

    push

--- components/agentForms/personResearchForm.tsx
+++ components/agentForms/personResearchForm.tsx
@@ -98,7 +98,7 @@ export default function PersonResearchForm({ imgSrc, agentInfo, agentText, agent
                                         ).join(' ')}</p>
                                     </div> */}
                                     <div className="flex flex-col gap-4">
-                                        <Markdown>{e.raw}</Markdown>
+                                        <div dangerouslySetInnerHTML={{__html:e.raw.replace('```html','').replaceAll('```','')}}></div>
                                     </div>
                                 </div>
                             </div>
